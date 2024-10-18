bad1d2f90ae015a8c23d180064aabae11e23bcf1
Jhon Deyby Priedrahita Higuita~ <jpiedhig@contratista.epm.co>
2024-10-18 14:54:40 -0500

    Update

--- HerramientasDeProgramacionII/obj/Debug/net8.0/HerramientasDeProgramacionII.AssemblyInfo.cs
+++ HerramientasDeProgramacionII/obj/Debug/net8.0/HerramientasDeProgramacionII.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HerramientasDeProgramacionII")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e9114cafff1d7d80479a745a1a830daefe3ff8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb5b13e03b68d87f220b4937684a3bdfc0fef37")]
 [assembly: System.Reflection.AssemblyProductAttribute("HerramientasDeProgramacionII")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HerramientasDeProgramacionII")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
