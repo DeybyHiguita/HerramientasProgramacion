9e20e6ec254f206d60c97113fbf4f85b2107e41e
Jhon Deyby Priedrahita Higuita~ <jpiedhig@contratista.epm.co>
2024-12-07 13:56:25 -0500

    Actualización de .gitignore

--- BaseDatos/Database/Database1/empleado_empleado.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE empleado.[empleado]
-(
-	idempleado INT PRIMARY KEY,
-    apellido VARCHAR(50) NOT NULL,
-    nombre VARCHAR(50) NOT NULL,
-    direccion VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL
-)
